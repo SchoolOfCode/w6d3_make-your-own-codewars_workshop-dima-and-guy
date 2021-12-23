8da4dfa858b89d5405151e698dbc88242576eab5
Dmitriy Yegorov <yegorovd14@gmail.com>
2021-12-23 10:09:01 +0000

    final kata

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+ 
 /* 
-👉 Write your kata here!
+👉  ******** COUNT LETTERS IN SENTENCE ********
+    
+    Hello Bootcamper! 
+    
+    In this simple kata you will need to count ONLY letters in a given string and return the next message `The sentence contains (NUMBER OF LETTERS) letters.`
+    The following is an example of data that will be passed in to the function. */
+    
+    const sentence = `Hello, welcome to School of Code!!!`
+
+   /* Good luck!)))
+
+    PS Please do not forget to 'npm i' all required dependencies before you test your solution.
+
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+        
+ export default function countLetters(sentence) {
+      // == Write your code here please 👉
+      return message;
+}
+ 
